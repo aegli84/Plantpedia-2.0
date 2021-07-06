10a0695d6945cab03cc580137df9eb6a4542b76b
maria <mariasventeriki@gmail.com>
2021-07-06 10:02:34 +0200

    biggerlogo-heightnavbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 const FooterBar = styled.div`
-height:25vh; 
+height:26vh; 
 width:100vw;
 background-color: black;
 `;
