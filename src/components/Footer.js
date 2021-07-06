a61d070b5634d1cf1d96711a7f628f8c789948aa
aegli84 <egli.andreea@yahoo.com>
2021-07-06 13:36:18 +0200

    x

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ margin-top: 20px;
 `;
 
 const IconsDiv = styled.div`
-padding:2rem 0 2rem 2rem;
+padding: 2rem 0 0rem 0✓rem;
 justify-content : space-between;
 text-align: center;
 align-content: center;
