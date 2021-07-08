dea6c342fb10125d0c1a90bcafaff57f72b4a749
NicklausDim <nikolas.dimitriadis@hotmail.com>
2021-07-08 11:52:43 +0200

    final MQ

--- src/components/plantpedia/WateringButton.js
+++ src/components/plantpedia/WateringButton.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 
 const WateringCard=styled.div`
-width:14vw;
-height:18vh;
+width:13vw;
+height:22vh;
 background-color: #725490;
 margin: auto;
 border-radius: 2rem;
@@ -13,7 +13,7 @@ display: flex;
     width:22vw;
     height:18vh;
 }
-@media (max-width: 400px) {
+@media (max-width: 377px) {
     grid-template-columns: auto auto ;
     width:30vw;
     height:14vh;
@@ -22,17 +22,17 @@ display: flex;
 `
 
 const InnerBlackFrame=styled.div`
-height:6vh;
+height:7vh;
 width: 14vw;
 background-color: #323144;
 border-radius: 0 0 2rem 2rem;
-margin-top:14vh;
+margin-top:15vh;
 @media (max-width: 770px) {
     grid-template-columns: auto auto;
     width:22vw;
     margin-top:12vh;
 }
-@media (max-width: 375px) {
+@media (max-width: 377px) {
     grid-template-columns: auto auto;
     width:30vw;
     height:5vh;
