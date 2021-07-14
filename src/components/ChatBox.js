5c1e5c9dcc6b548cb601b02d11373378ac67beb2
aegli84 <egli.andreea@yahoo.com>
2021-07-14 23:22:13 +0200

    chat v3 a/ finished

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,97 +1,40 @@
 //import styled from "styled-components";
-import { useRef, useState, useEffect } from 'react';
+//import { useRef, useState, useEffect } from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './Chat.css';
-import { useHistory } from 'react-router-dom';
-import { auth } from '../components/firebase'
+
+
  //import { AuthProvider } from '../context/AuthContext'
-import { useAuth } from '../context/AuthContext'
-import axios from 'axios'
+
+//import axios from 'axios'
 
 
 const ChatBox = () => {
-    const didMountRef = useRef(false)
-    const history = useHistory()
-    const { user } = useAuth()
-    const [loading, setLoading] =useState(true)
-
-    const handleLogout = async () => {
-        await auth.signOut()
-        history.push("/")
-    }
-
-    const getFile = async (url) => {
-        const response = await fetch(url);
-        const data = await response.blob();
-
-        return new File([data], 'userPhoto.jpeg', {type: 'image/jpeg'})
-    }
-    useEffect (() => {
-        if (!didMountRef.current) {
-            didMountRef.current = true
     
-            if (!user || user === null) {
-                history.push("/")
-                return
-            }
-        }
-                //trying to get the exisiting user
-        axios.get('https://api.chatengine.io/users/me/', {
-            headers: {
-                'project-id': process.env.REACT_APP_CHAT_ENGINE_ID,
-                'user-name': user.email,
-                'user-secret': user.uid,
-            }
-        })
-        .then(() => {
-            setLoading(false)
-        })
-        .catch(() => {
-            let formdata = new FormData();
-            formdata.append('email', user.email)
-            formdata.append('username', user.email)
-            formdata.append('secret', user.uid)
-
-            getFile(user.photoURL)
-                .then((avatar) => {
-                    formdata.append('avatar', avatar, avatar.name)
-
-                    axios.post('http://api.chatengine.io/users/',
-                        formdata,
-                        { headers: {'private-key': process.env.REACT_APP_CHAT_ENGINE_KEY}}
-                    )
-                        .then(() => setLoading(false))
-                        .catch(e => console.log('e', e.response))
-                })
-        })
-    }, [user, history])
-
-
-    if(!user || loading)  return 'Loading, please wait...'
-
     return(
-        <>
+        <div id='wrapper'>
         
         <div className='chats-page'>
             <div className='nav-bar'>
                 <div className='logo-tab'>
-                    Plantpedia chat
+                    Plantpedia Chat
             </div>
-            <div onClick={handleLogout} className='logout-tab'>
+            {/* <div  className='logout-tab'>
                     Logout
-            </div>
+            </div> */}
         </div>
+
                 <ChatEngine
-                height='calc(80vh - 33px)'
-                projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
-                userName={user.email}
-                userSecret={user.uid}
+                height='70vh'
+                with='70%'
+                projectID='cf898989-697a-41cf-a92e-fa5161403740'
+                userName='aegli'
+                userSecret='123'
                 onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
                 />
-            
         </div>
         
-        </>
+        </div>
     )
 }
 
