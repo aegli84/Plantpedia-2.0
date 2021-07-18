94fd5566421d0a1edb57864d8ce8d9ced6038a5b
aegli84 <egli.andreea@yahoo.com>
2021-07-18 14:02:11 +0200

    logout fixed

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,5 +1,5 @@
 //import styled from "styled-components";
-import { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './Chat.css';
 import { useHistory } from 'react-router-dom';
@@ -12,16 +12,14 @@ import {FiLogOut } from "react-icons/fi"
 
 const ChatBox = () => {
     
-    // if(!localStorage.getItem('username')) return <LoginForm />
-
-    const didMountRef = useRef(false)
+    //const didMountRef = useRef(false)
     const history = useHistory()
     const { user } = useAuth()
     const [loading, setLoading] =useState(true)
 
-    const handleLogout = async () => {
+    async function handleLogout() {
         await auth.signOut()
-        history.push("/login") // re-navigates to the login form after logout
+        history.push("/Chat")
     }
 
     const getFile = async (url) => {
@@ -31,14 +29,14 @@ const ChatBox = () => {
         return new File([data], 'userPhoto.jpeg', {type: 'image/jpeg'})
     }
     useEffect (() => {
-        if (!didMountRef.current) {
-            didMountRef.current = true
+        // if (!didMountRef.current) {
+        //     didMountRef.current = true
     
             if (!user || user === null) {
                 history.push("/")
                 return
             }
-        }
+        
                 //trying to get the exisiting user
                 axios.get(
                     'https://api.chatengine.io/users/me/',
@@ -80,9 +78,18 @@ const ChatBox = () => {
     if(!user || loading)  return <div className='tracking-in-expand' style = {{textAlign: 'center', color: 'whitesmoke', display: 'block', fontSize: '1.8rem'}}>Chat loading, please wait...</div>
 
     return(
-        <div id='wrapper'>
+        <div>
+        
             <div className='chats-page'>
-                <div className='nav-bar'>
+            <div className='nav-bar-main' >
+                    <div onClick={handleLogout} className='logout-tab'>
+                            <FiLogOut size = '2rem'/>
+                    </div>
+                    <div className='logo-tab'>
+                        Plantpedia Chat
+                    </div>
+            </div>
+                <div className='nav-bar' >
                     <div onClick={handleLogout} className='logout-tab'>
                             <FiLogOut size = '2rem'/>
                     </div>
