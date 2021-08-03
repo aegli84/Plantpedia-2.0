8466cd3de7a330128c6a8b483ba66fb52d954beb
NicklausDim <nikolas.dimitriadis@hotmail.com>
2021-08-03 15:18:50 +0200

    guides ready and maps ready

--- src/components/plantpedia/plantpediaAssets/plantpediaData.js
+++ src/components/plantpedia/plantpediaAssets/plantpediaData.js
@@ -35,7 +35,7 @@ const plants = [
     {
         id:"4",
         photo:"as",
-        plantName:"Zamloculcas plant",
+        plantName:"Zamloculcas",
         description:"The alocasia zebrina owes its name to the unique – and eye-catching – yellow and black zebra print-esque markings found on its stems that contrast with its lush green leaves. Its leaves are in fact believed to mirror the floppy ears of an elephant – hence the alocasia zebrina’s other name, ‘elephant ear’.ts leaves are as fascinating as its stems, thanks to their funnel-like concavity, and pronounced veins that are not unlike those seen on a human body. Originally from Southeast Asia, the exotic alocasia zebrina is familiar with the rainforest floor – meaning it loves a moist, humid environment – and its large leaves will follow the path of light (so be sure to rotate yours once in while).Make sure your alocasia is not near your pets!",
         humidity:"55%-60%",
         watering: "Moist but not soggy",
