b4a9a5eaad906d5b08b37f4c420a7d879cdacc6e
NicklausDim <nikolas.dimitriadis@hotmail.com>
2021-08-03 13:31:39 +0200

    map and guides finished

--- src/components/plantpedia/WateringButton.js
+++ src/components/plantpedia/WateringButton.js
@@ -53,7 +53,7 @@ const Thumbnail = styled.div`
     background-color: white;
     border-radius: 50%;
     margin-top: 2vh;
-    margin-left: 6.5vh;
+    margin-left: 51px;
 @media (max-width: 770px) {
     width:14vw;
     height: 10vh;
