406ae29548dca05a5f773f8c1a96d25403f9f3a0
aegli84 <egli.andreea@yahoo.com>
2021-08-10 20:33:30 +0200

    test push

--- src/components/CheckoutForm/CustomTextField.js
+++ src/components/CheckoutForm/CustomTextField.js
@@ -3,7 +3,7 @@ import { TextField, Grid } from '@material-ui/core';
 
 const FormInput = ({name, label, required}) => {
     const {control} =useFormContext();
-    //const isError = false;
+
 
     return (
         <Grid item xs={12} sm={6}>
