8c583109a7ec8c5b85d5862fda1dce18ee64a027
aegli84 <egli.andreea@yahoo.com>
2021-08-11 16:21:44 +0200

    contact page & form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,20 +1,40 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 import { useForm, ValidationError } from '@formspree/react';
+import Grid from "react-fast-grid";
 
 const ContactForm = () => {
     const [state, handleSubmit] = useForm(process.env.REACT_APP_CONTACT_FORM_KEY);
     if (state.succeeded) {
-        return <p>Thank!</p>;
+        return <div>Thank!</div>;
     }
+
+    const styles = {
+        outer: {
+            borderRadius: '.6rem',
+            boxShadow: "0 10px 30px black",
+            paddingLeft: '7rem', 
+            // paddingRight: '5rem', 
+            paddingTop: 25,
+            paddingBottom: 25,
+            marginBottom: 50,
+            marginLeft: 10,
+            height: '72vh',
+            width: '85vw'
+            },
+        };
+
     return (
         <>
-            <h1>Contact Us</h1>
+        <div style={styles.outer}>
             <form onSubmit={handleSubmit}>
-                <input 
+            <Grid container spacing={2} sm={10} direction="row">
+            <Grid item sm={6} xs={12}>
+            <Label>First Name</Label>
+                <Input 
                     id="name"
                     name = "name" 
                     type = "text" 
-                    placeholder ="Full Name" 
+                    placeholder ="" 
                     required= {true}
                     />
                     <ValidationError 
@@ -22,69 +42,167 @@ const ContactForm = () => {
                         field="name"
                         errors={state.errors}
                     />
-                <input 
-                    id="email"
-                    name = "email" 
+                    </Grid>
+
+            
+            
+            <Grid item sm={6} xs={12}>
+            <Label>Last Name</Label>
+                <Input 
+                    id="name"
+                    name = "name" 
                     type = "text" 
-                    placeholder ="you@email.com" 
+                    placeholder ="" 
                     required= {true}
                     />
                     <ValidationError 
+                        prefix="Name"
+                        field="name"
+                        errors={state.errors}
+                    />
+                </Grid>
+                
+            
+            <Grid item sm={6} xs={12}>
+            <Label>Email</Label>
+                <Input 
+                    id="email"
+                    name = "email" 
+                    type = "text" 
+                    placeholder ="" 
+                    required= {true}
+                    />
+                    <ValidationError 
+                        style={{color: 'red', fontFamily: 'Montserrat', paddingTop: '1rem'}}
                         prefix="Email" 
                         field="email"
                         errors={state.errors}
                     />
-                <input 
+                    </Grid>
+                <Input 
                     type="text" 
                     name="_gotcha" 
                     style= {{display:'none'}} 
                     />
-                <textarea 
+                    <Grid item sm={6} xs={12}>
+                        <Label>Reason for contact</Label>
+                        <Select name="reason-for-contact" required>
+                            <Option value="">-- Please choose an option --</Option>
+                            <Option value="plantpedia">Plantpedia</Option>
+                            <Option value="subscription">Subscription</Option>
+                            <Option value="chat">Chat</Option>
+                            <Option value="account">Account</Option>
+                        </Select>
+                    </Grid>
+                <Grid item sm={6} xs={12}>
+                <Label>Message</Label>
+                <Textarea 
                     id = "message"
                     type = "text" 
                     name = "message" 
                     cols = "25" 
                     rows = "7" 
-                    placeholder= "Your message here"
+                    placeholder= ""
                     required= {true}
                     >
-                </textarea>
+                </Textarea>
                     <ValidationError 
                         prefix="Message" 
                         field="message"
                         errors={state.errors}
                         />
-                <button 
-                    className = "button"
-                    id = "submit" 
-                    type = "submit" 
-                    value = "submit"
-                    disabled={state.submitting}
-                    >Send
-                </button>
+                    <Button 
+                        className = "button"
+                        id = "submit" 
+                        type = "submit" 
+                        value = "submit"
+                        disabled={state.submitting}
+                        >Send
+                    </Button>
+                </Grid>
+            </Grid>
             </form>
+        </div>
         </>
     )
 }
 
-// const Div =styled.div`
-//     min-height: 100vh;   
-//     width: 100%;
-//     background-image: url(${plantname});
-//     background-repeat: no-repeat;
-//     background-size: cover;
 
-/* 
-@media (max-width: 768px) {
-    min-height: 69vh;  
-    margin-top: 5rem;
+const Label = styled.label `
+    color: whitesmoke;
+    display: block;
+    padding: .7rem;
+    font-size: 1.2rem;
+    font-family: 'Montserrat', sans-serif !important;
+`
+const Input = styled.input `
+    border-radius: .5rem;
+    padding: 1rem;
+    background-color: #8978a3;
+    opacity: 0.9;
+    cursor: pointer;
+    border: none;
+    outline:none;
+    width: 100%;
+    &:hover {
+        border: 3px solid #83a46f;
+    }
+`
+const Select = styled.select `
+    border-radius: .5rem;
+    padding: 1rem;
+    background-color: #8978a3;
+    border: none;
+    outline: none;
+    opacity: 0.9;
+    width: 100%;
+    color: whitesmoke;
+    font-family: 'Montserrat', sans-serif !important;
+    &:hover {
+        border: 3px solid #83a46f;
+    }
+`
+const Textarea = styled.input `
+    border-radius: .5rem;
+    background-color: #8978a3;
+    padding: 1rem;
+    outline: none;
+    opacity: 0.9;
+    cursor: pointer;
+    border: none;
+    width: 28vw;
+    height: 20vh;
+    &:hover {
+        border: 3px solid #83a46f;
+    }
     
-}
-
-@media (max-width: 480px) {
-    min-height: 70vh;
-    padding-bottom: 2rem;
-    background-size: contain;
-} */
-
+`
+const Button = styled.button `
+    color: whitesmoke;
+    display: block;
+    border-radius: .5rem;
+    outline:none;
+    cursor: pointer;
+    border: none;
+    padding: 1rem;
+    margin-top: 1.7rem;
+    width: 50%;
+    background-color: #83a46f;
+    text-transform: uppercase;
+    font-weight: 500;
+    font-family: 'Montserrat', sans-serif !important;
+    &:hover {
+        background-color: #8978a3;
+        transition: all 0.4s;
+        opacity: .9;
+    }
+`
+const Option = styled.option `
+    color: whitesmoke;
+    border-radius: .5rem;
+    outline:none;
+    border: none;
+    font-weight: 500;
+    font-family: 'Montserrat', sans-serif !important;
+`
 export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,31 @@
 import styled from "styled-components";
-import plantname from '../assets/bgimg/plantname.png'
+import contactpage from '../assets/bgimg/contactpage.png'
 import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
     return (
         <Div>
-           <ContactForm/>
+        <div>
+        
+        <H1>Contact Us.</H1>
+            <P>Our team is full of passionate plant parents ready to help. For the speediest reply, please fill out this quick form.</P>
+            <P>This ensures we have what’s needed to help you as soon as possible!</P>
+            
+    </div>
+            <ContactForm/>
         </Div>
     )
 }
 
 const Div =styled.div`
-    min-height: 100vh;   
-    width: 100%;
-    background-image: url(${plantname});
+    height: 90vh;   
+    width: 95%;
+    background-image: url(${contactpage});
     background-repeat: no-repeat;
     background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 5rem;
 
 /* 
 @media (max-width: 768px) {
@@ -30,4 +40,43 @@ const Div =styled.div`
     background-size: contain;
 } */
 `
+
+const H1 = styled.h1`
+    text-align: left;
+    margin-left: 5rem;
+    color: whitesmoke;
+    padding-top: 2rem;
+    font-size: 3.5rem;
+    font-family: 'Montserrat', sans-serif !important;
+    text-shadow: 0  15px 7px black; 
+
+/* @media (max-width: 768px) {
+    padding-top: 3rem;
+}
+
+@media (max-width: 480px) {
+    padding-top: 1.5rem;
+    font-size: 5rem;
+} */
+`
+const P = styled.p`
+    /* text-align: center; */
+    color: whitesmoke;
+    margin: 0 5rem;
+    padding-top: 2rem;
+    font-size: 1.3rem;
+    font-weight: 400;
+    font-family: 'Montserrat', sans-serif !important;
+
+/* @media (max-width: 768px) {
+    margin: 1.5rem 6rem;
+    font-size: 1.6rem;
+}
+
+@media (max-width: 480px) {
+    margin: 1rem 2rem;
+    font-size: 1.6rem;
+} */
+`
+
 export default Contact;
