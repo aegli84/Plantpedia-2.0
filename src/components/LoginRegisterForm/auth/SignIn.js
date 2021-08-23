52a9e88cb33c9cb2db9b635b3245f3fabe95e566
maria <mariasventeriki@gmail.com>
2021-08-23 16:46:43 +0200

    signinfinal

--- src/components/LoginRegisterForm/auth/SignIn.js
+++ src/components/LoginRegisterForm/auth/SignIn.js
@@ -53,19 +53,19 @@ const Signin = ({ history }) => {
     };
 
     const signinForm = () => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} value={email} type="email" className="form-control" />
+        <form className="main-form">
+            <div className="form-groups">
+                <label className="label-head">Email</label>
+                <input onChange={handleChange('email')} value={email} type="email" className="input-con" />
             </div>
 
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} value={password} type="password" className="form-control" />
+            <div className="form-groups">
+                <label className="label-head">Password</label>
+                <input onChange={handleChange('password')} value={password} type="password" className="input-con" />
             </div>
 
             <div>
-                <button className="btn btn-primary" onClick={clickSubmit}>
+                <button className="btn-send" onClick={clickSubmit}>
                     {buttonText}
                 </button>
             </div>
@@ -73,16 +73,19 @@ const Signin = ({ history }) => {
     );
 
     return (
-        <div>
-            <div className="col-md-6 offset-md-3">
+        <div className="main">
+            <div className="secondDiv">
                 <ToastContainer />
                 {isAuth() ? <Redirect to="/" /> : null}
-                <h1 className="p-5 text-center">Signin</h1>
+                <div className="header">
+                    <h2>Login</h2>
+                    <h1>Enter your account.</h1>
+                    <h3>You don´t have an account yet?<Link to = "/signup" style={{textDecoration: "underline" , color:"#83a46f"}}>Create one here</Link></h3>
+                </div>
                 {/* <Google informParent={informParent} />
                 <Facebook informParent={informParent} /> */}
                 {signinForm()}
-                <br />
-                <Link to="/auth/password/forgot" className="btn btn-sm btn-outline-danger">
+                <Link to="/auth/password/forgot" className="btn-forgot">
                     Forgot Password
                 </Link>
             </div>
