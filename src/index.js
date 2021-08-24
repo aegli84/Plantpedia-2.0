2fbe780d68c5e1bcf22f83ae78858cb2927ecc7a
aegli84 <egli.andreea@yahoo.com>
2021-08-24 13:27:36 +0200

    minor change/delete

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter >
-    {/* <ScrollToTop/> */}
       <App />
-
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
